1df002dbd4dded94060f72236ef8ece5516d8886
Carmen Benavente <cbenavente@miuandes.cl>
2020-05-07 09:08:26 -0400

    Class Person

---
+++ Series/Series/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Series
+{
+    public class Person
+    {
+        string Name;
+        string LastName;
+        string Age;
+
+        public string Name1 { get => Name; set => Name = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+        public string Age1 { get => Age; set => Age = value; }
+
+        Person(string name, string lastname, string age)
+        {
+            name = Name;
+            lastname = LastName;
+            age = Age;
+        }
+    }
+}
---
+++ Series/Series/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           // menu crear persona
+             //   ver personas
+               //  almacenar cargar
+        }
+    }
+}
